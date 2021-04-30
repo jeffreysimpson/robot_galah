0574153ec62dbcb7637af1c3aa605c3f26b44c41
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2021-04-30 10:41:17 +1000

    Changed to the hips2fits service for the images.
    There is now a check that the star is actually within a survey 
    footprint, rather than trying to download and failing as before.
    
    The images are also slightly nicer looking, without the obvious lines of 
    the tiles.

--- do_the_tweeting.py
+++ do_the_tweeting.py
@@ -51,7 +51,7 @@ def tweet(tweet_text, hips_survey, gaia_dr3_id):
         logger.error(e)
         return 1
 
-    alt_text_dict = {"sky_image_overlay.jpg": f"A 15 by 15 arcminute image from the {hips_survey.split('/')[2]}. Gaia eDR3 {gaia_dr3_id} is found at the centre.",
+    alt_text_dict = {"sky_image_overlay.jpg": f"A 15 by 15 arcminute image from the {hips_survey}. Gaia eDR3 {gaia_dr3_id} is found at the centre.",
                      "stellar_params_teff.png": f"Two graphs made from GALAH survey data. The top panel is a temperature versus surface gravity, and the bottom panel is the Tinsley-Wallerstein diagram showing the metallicity versus the alpha abundance. On both, Gaia eDR3 {gaia_dr3_id} is indicated with a big red star.",
                      "stellar_params_L_Z.png": f"Two graphs made from GALAH survey data. The top panel is the z-component of the angular momentum versus the orbital energy. The bottom panel is the Toomre diagram. On both, Gaia eDR3 {gaia_dr3_id} is indicated with a big red star.",
                      "spectra.png": f"The normalized HERMES spectrum of Gaia eDR3 {gaia_dr3_id}. HERMES acquires the spectrum of the star in four non-contiguous wavelength regions: Blue, Green, Red, and Infrared."}
--- get_images.py
+++ get_images.py
@@ -1,15 +1,47 @@
 import logging
 import logging.config
+import shutil
 from pathlib import Path
-from urllib.error import HTTPError, URLError
+from urllib.parse import quote
 
-from astropy.coordinates import SkyCoord
-from hips import WCSGeometry, make_sky_image
+import requests
+from astropy import units as u
+from mocpy import MOC
 from PIL import Image, ImageDraw, ImageFont
 
 
-def get_hips_image(the_star):
+def within_footprint(survey_url, the_star):
+    """Checks if the star is within the footprint of a given survey."""
+    survey_moc = MOC.from_fits(f"{survey_url}/Moc.fits")
+    return survey_moc.contains(the_star['ra'] * u.degree,
+                               the_star['dec'] * u.degree)[0]
 
+
+def download_image(survey_url, the_star, logger, base_image):
+    """Downloads the HiPS image."""
+    width = 1000
+    height = 1000
+    fov = 0.25
+    url = 'http://alasky.u-strasbg.fr/hips-image-services/hips2fits?hips={}&width={}&height={}&fov={}&projection=TAN&coordsys=icrs&ra={}&dec={}&format=jpg&stretch=power'.format(
+        quote(survey_url), width, height, fov, the_star['ra'], the_star['dec'])
+    logger.debug("Trying %s", url)
+    response = requests.get(url, stream=True)
+
+    if response.status_code < 400:
+        logger.debug("HTTP response: %s", response.status_code)
+        with open(base_image, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+            logger.info("Saved the image to %s", base_image)
+        del response
+        return 0
+    else:
+        logger.error("BAD HTTP response: %s", response.status_code)
+        del response
+        return 1
+
+
+def get_hips_image(the_star):
+    """Main function to get a sky image for the given star."""
     cwd = Path(__file__).parent
     tweet_content_dir = Path.joinpath(cwd, "tweet_content")
     config_file = Path.joinpath(cwd, 'logging.conf')
@@ -19,35 +51,28 @@ def get_hips_image(the_star):
 
     gaia_dr3_id = the_star['dr3_source_id']
 
-    geometry = WCSGeometry.create(
-        skydir=SkyCoord(the_star['RA'], the_star['Dec'], unit='deg'),
-        width=1000, height=1000, fov="15 arcmin", projection='AIT',
-    )
-    logger.info("Getting the images")
-    for hips_survey in ['CDS/P/PanSTARRS/DR1/color-z-zg-g',
-                        'NOAO/P/DES/DR1/LIneA-color',
-                        'CDS/P/DSS2/color',
-                        # 'CDS/P/Skymapper-color-IRG'
-                        ]:
-        logger.info("Trying %s", hips_survey)
-        try:
-            result = make_sky_image(geometry, hips_survey, 'jpg')
-        except HTTPError:
-            logger.info("No useful images from %s", hips_survey)
-            continue
-        except URLError as e:
-            logger.error(e)
-            return 1
-        logger.info("Succesfully downloaded the sky image")
-        base_image = Path.joinpath(tweet_content_dir, "sky_image.jpg")
-        result.write_image(base_image)
-        logger.info("Saved image to %s", base_image)
-        break
+    ohips = [['Pan-STARRS', 'http://alasky.u-strasbg.fr/Pan-STARRS/DR1/color-z-zg-g'],
+             ['DECaLS',  'http://alasky.u-strasbg.fr/DECaLS/DR5/color'],
+             ['DSS2',  "http://alasky.u-strasbg.fr/DSS/DSSColor"]]
+
+    base_image = Path.joinpath(tweet_content_dir, "sky_image.jpg")
+
+    for survey, survey_url in ohips:
+        logger.info("Trying %s", survey)
+        if within_footprint(survey_url, the_star):
+            logger.info("Target is within footprint")
+            res = download_image(survey_url, the_star, logger, base_image)
+            if res == 0:
+                break
 
     logger.info("Adding the overlay")
     font = ImageFont.truetype(
         "/Users/jeffreysimpson/Library/Fonts/Roboto-Bold.ttf", 40)
-    img_sky = Image.open(base_image)
+    try:
+        img_sky = Image.open(base_image)
+    except FileNotFoundError as e:
+        logger.error(e)
+        return 1
     draw = ImageDraw.Draw(img_sky, "RGBA")
     draw.line([((500 - 80), 500),
                ((500 - 20), 500)], fill='white', width=5)
@@ -57,9 +82,9 @@ def get_hips_image(the_star):
                (815 + 1000 / 15 * 2, (1000 - 70))], fill='white', width=5)
     draw.text((30, 10), f"Gaia eDR3 {gaia_dr3_id}", (255, 255, 255), font=font)
     draw.text((30, (1000 - 60)),
-              f"{hips_survey.split('/')[2]}", (255, 255, 255), font=font)
+              f"{survey}", (255, 255, 255), font=font)
     draw.text((800, (1000 - 60)), "2 arcmin", (255, 255, 255), font=font)
     overlayed_image = Path.joinpath(tweet_content_dir, "sky_image_overlay.jpg")
     img_sky.save(overlayed_image)
     logger.info("Saved overlayed image to %s", overlayed_image)
-    return hips_survey
+    return survey
