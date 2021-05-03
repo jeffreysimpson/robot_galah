b444e797296046efd10f79c4994818d871204e77
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2021-05-03 14:01:45 +1000

    Fix if there is missing spectra.

--- plot_spectra.py
+++ plot_spectra.py
@@ -12,6 +12,7 @@ import pandas as pd
 from astropy.constants import c
 from astropy.io import fits
 from matplotlib import rcParams
+from matplotlib.offsetbox import AnchoredText
 from pyvo.dal.exceptions import DALFormatError, DALServiceError
 from pyvo.dal.ssa import SSAService
 
@@ -50,21 +51,16 @@ def plot_spectra(the_star):
     indiv_results.append(results.votable.get_first_table().to_table(
         use_names_over_ids=True).to_pandas())
 
+    bands_names = ["B", "V", "R", "I"]
+
+    band_dict = {"B": {"color": "C4", "ticks": np.arange(4730, 4900, 75), "name": "blue"},
+                 "V": {"color": "C0", "ticks": np.arange(5680, 5900, 75), "name": "green"},
+                 "R": {"color": "C3", "ticks": np.arange(6500, 6900, 75), "name": "red"},
+                 "I": {"color": "C2", "ticks": np.arange(7610, 7900, 75), "name": "infrared"}}
+
     df = pd.concat(indiv_results, ignore_index=True)
 
     plot_list_base = [[i] for i in ['B', 'V', 'R', 'I']]
-
-    colour_dict = {"B": "C4",
-                   "V": "C0",
-                   "R": "C3",
-                   "I": "C2"
-                   }
-
-    ticks_dict = {"B": np.arange(4730, 4900, 75),
-                  "V": np.arange(5680, 5900, 75),
-                  "R": np.arange(6500, 6900, 75),
-                  "I": np.arange(7610, 7900, 75),
-                  }
 
     rv_correction = (
         c / ((the_star['rv_galah'] * u.km / u.s) + c)).decompose().value
@@ -88,11 +84,12 @@ def plot_spectra(the_star):
                              len(spec[0].data))
             axes[spec_row['band_name']].plot(wl * rv_correction,
                                              spec[0].data,
-                                             c=colour_dict[spec_row['band_name']],
+                                             c=band_dict[spec_row['band_name']
+                                                         ]['color'],
                                              lw=0.5)
 
         redo_axes_list[spec_row['band_name']].update(
-            {"xticks": ticks_dict[spec_row['band_name']],
+            {"xticks": band_dict[spec_row['band_name']]['ticks'],
              "yticks": [],
              "xlim": np.percentile(wl, [0, 100]) + [-3, 3],
              "ylim": [0, 1.2]
@@ -104,6 +101,20 @@ def plot_spectra(the_star):
 
     axes['B'].set_title(
         f"Normalized HERMES spectrum of\nGaia eDR3 {the_star['dr3_source_id']}")
+
+    for missing_band in [band for band in bands_names if band not in df['band_name'].to_list()]:
+        redo_axes_list[missing_band].update(
+            {"xticks": [],
+             "yticks": [],
+             "ylim": [0, 1.2]
+             })
+        logger.info("Missing the spectra for the %s camera", missing_band)
+        anchored_text = AnchoredText(f"This star does not have an {band_dict[missing_band]['name']} camera spectrum.",
+                                     loc='center',
+                                     frameon=False, pad=0,
+                                     prop=dict(color=band_dict[missing_band]['color']))
+        axes[missing_band].add_artist(anchored_text)
+
     galah_plotting.redo_plot_lims(axes, redo_axes_list)
     spec_file = Path.joinpath(tweet_content_dir, "spectra.png")
     logger.info("Saving spectrum to %s", spec_file)
