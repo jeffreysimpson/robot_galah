9a8e23b530bd6b05482bf4e673948ec469d1f2ac
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2021-05-13 08:28:16 +1000

    Fix DES link

--- get_images.py
+++ get_images.py
@@ -69,7 +69,7 @@ def get_hips_image(the_star, secrets_dict):
     gaia_dr3_id = the_star['dr3_source_id']
 
     ohips = [['DECaLS DR5',  'http://alasky.u-strasbg.fr/DECaLS/DR5/color'],
-             ['DES DR1', "http://datalab.noao.edu/HiPS/des_dr1/Moc.fits"],
+             ['DES DR1', "http://alasky.u-strasbg.fr/DES/CDS_P_DES-DR1_ColorIRG"],
              ['PanSTARRS-1', 'http://alasky.u-strasbg.fr/Pan-STARRS/DR1/color-z-zg-g'],
              ['DSS2',  "http://alasky.u-strasbg.fr/DSS/DSSColor"]
              ]
